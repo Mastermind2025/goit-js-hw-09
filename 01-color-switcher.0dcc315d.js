bec729edd0f0718985da9954b093078b2b461a65
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-11-09 12:50:04 +0000

    Deploying to gh-pages from @ Mastermind2025/goit-js-hw-09@0c10488bfb29e8b11a572a46f847907de4b8b8c3 🚀

--- 01-color-switcher.0dcc315d.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){n=setInterval(o,500)})),e.addEventListener("click",(function(){clearInterval(n)}));let n,o=function(){return document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};
-//# sourceMappingURL=01-color-switcher.0dcc315d.js.map
---
+++ 01-color-switcher.bb032972.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){e=setInterval(o,700)})),n.addEventListener("click",(function(){clearInterval(e)}));var e=null,o=function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
+//# sourceMappingURL=01-color-switcher.bb032972.js.map
