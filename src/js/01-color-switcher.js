0c10488bfb29e8b11a572a46f847907de4b8b8c3
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-11-09 14:49:36 +0200

    process

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,16 +2,15 @@ const btnStart = document.querySelector('button[data-start]');  //Start button o
 const btnStop = document.querySelector('button[data-stop]');    //Stop button obj
 btnStart.addEventListener('click', onClickColor);   //add Listener to Start button
 btnStop.addEventListener('click', cancelColor);     //add Listener to Stop button
-
+let cancelInterval = null;
 //The function generates random color and assigns to body
 let getRandomHexColor = function(){
   return document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 } 
 
-let cancelInterval;
 // We run interval by click on Start button with color function
 function onClickColor() {
-    cancelInterval = setInterval(getRandomHexColor, 500);
+    cancelInterval = setInterval(getRandomHexColor, 700);
 }
 // Stop interval function by click Stop button
 function cancelColor() {
@@ -21,18 +20,3 @@ function cancelColor() {
 
 
 
-// console.log(intervalSet);
-
-// function endInterval() {
-//     clearInterval(intervalSet);
-//     console.log('End!');
-// }
- 
-// const intervalSet = setInterval((funColor) => {
-//     funColor = console.log('Hi!');
-
-// }, 1000);
-
-
-// console.log(getRandomHexColor());
-// document.body.style.backgroundColor = 'green';
